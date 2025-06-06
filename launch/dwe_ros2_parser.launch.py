c7a53fc6cc4cad724165104e3d0d09cdc03631de
xelisce <xelisce@gmail.com>
2025-06-06 18:06:45 +0800

    Working version on Jetson after changes

--- launch/dwe_ros2_parser.launch.py
+++ launch/dwe_ros2_parser.launch.py
@@ -34,7 +34,7 @@ def generate_launch_description():
         package='dwe_ros2_parser',
         executable='dwe_ros2_parser',
         parameters=[{
-          'device': 9,
+          'device': 0,
           'image_topic': '/dwe/camera',
           'width': 800,
           'height': 600,
@@ -43,8 +43,8 @@ def generate_launch_description():
           'exposure': 100,
           'show_image' : True, 
           'use_h264': False,
-          'save_images': True,
-          'save_folder': '/home/urlaxle/data/camera_calibration/17092024/left',
+          'save_images': False,
+          'save_folder': '/home/gg/camera',
           'image_prefix': 'left',
         }],
         output='screen'
